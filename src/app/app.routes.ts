02b32922611cfb9beaf4fd60c5ffac37941be81b
Andre McMorris <ajmcmorris@gmail.com>
2025-11-14 00:10:05 -0500

    added common component and basic app setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PoDetail } from './domains/purchase_order/components/po-detail/po-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'purchase',
+        pathMatch: 'full'
+    },
+    {
+        path: 'purchase',
+        component: PoDetail,
+        title: 'Purchase Order Detail'
+    }
+];
