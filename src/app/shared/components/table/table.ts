548429cd205e5afdb4d1b6328e943ea30f9ab2d0
Andre McMorris <ajmcmorris@gmail.com>
2025-11-14 13:08:16 -0500

    added focus state, added title to table component

--- src/app/shared/components/table/table.ts
+++ src/app/shared/components/table/table.ts
@@ -13,6 +13,7 @@ export interface TableColumn {
 export class Table<T> {
   @Input() data: T[] = [];
   @Input() columns: TableColumn[] = [];
+  @Input() tableName = '';
 
   get headers(): string[] {
     // If columns are provided, use them; otherwise auto-generate from data
