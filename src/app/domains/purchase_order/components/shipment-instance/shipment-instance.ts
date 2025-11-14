34a4e391be20eabf6c53b7c93ddafae77b3fdf10
Andre McMorris <ajmcmorris@gmail.com>
2025-11-14 13:00:18 -0500

    purchase order component enhancements

--- src/app/domains/purchase_order/components/shipment-instance/shipment-instance.ts
+++ src/app/domains/purchase_order/components/shipment-instance/shipment-instance.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ShipmentInstanceModel } from '../../models/shipment_instance.model';
 import { Accordion } from '../../../../shared/components/accordion/accordion';
+import { AlertBanner } from '../../../../shared/components/alert-banner/alert-banner';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-shipment-instance',
-  imports: [Accordion],
+  imports: [Accordion, AlertBanner, DatePipe],
   templateUrl: './shipment-instance.html',
   styleUrl: './shipment-instance.css',
 })
--- src/app/domains/purchase_order/components/shipment/shipment.ts
+++ src/app/domains/purchase_order/components/shipment/shipment.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Card } from '../../../../shared/components/card/card';
 import { ShipmentModel } from '../../models/shipment.model';
 import { ShipmentInstance } from '../shipment-instance/shipment-instance';
+import { AlertBanner } from '../../../../shared/components/alert-banner/alert-banner';
 
 @Component({
   selector: 'app-shipment',
-  imports: [Card, ShipmentInstance],
+  imports: [Card, ShipmentInstance, AlertBanner],
   templateUrl: './shipment.html',
   styleUrl: './shipment.css',
 })
